f7921bc8e21ac30d3867bc4b0000455cf26a105f
psyfer <psyferblock.io@gmail.com>
2022-06-02 00:15:05 +0300

    deleted javascript and started over again

--- todo.js
+++ todo.js
@@ -1,46 +1,32 @@
 const list_container=document.getElementById('task-list');
 const get_todo_elements=document.getElementById("get-todo-elements");
+let input_field=document.getElementById("input-field");
 
 const todo_items =[];
 
-
-function render() {
-    // clearElement(list_container);
-    todo_items.forEach((list)=>{
-        clearElement(list_container);
-        const list_element=document.createElement("li");
-        const text=list.hasOwnProperty(text);
-        console.log(text);
-        const id =list.hasOwnProperty(id);
-        list_element.className="list-name";
-        list_element.innerHTML.appendChild(text,id)
-
-        list_container.appendChild(list_element);   
-    });
-
-}
-
 get_todo_elements.addEventListener("click",(e) =>{
     e.preventDefault();
-    const input_field=document.getElementById("new-list");
-    console.log(input_field)
-    const text=input_field.value;
-    console.log(text)
-    addTodo(text)
+    let text_input=inputfield.textContent;
+    todo_items.push(text_input);
+    console.log(todo_items)
+    debugger
+    addTodo()
     render()
     
 });
 
+
+
+
 function addTodo(text) {
     const todo = {
       text,
-      checked: false,
       id: Date.now().toString(),
     };
    
   
     todo_items.push(todo);
-    console.log(todo_items);
+    return todo
 };
 
 function clearElement(element){
@@ -49,3 +35,13 @@ function clearElement(element){
     }
 }
   
+function render() {
+    // clearElement(list_container);
+    todo_items.forEach((list)=>{
+        clearElement(list_container);
+        const list_element=document.createElement("li");        
+        list_element.className="list-name";
+        list_element.appendChild(list.value);
+
+    });
+};
