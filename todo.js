1d885617936d856089c1c0c29c6837b8d130c6cb
psyfer <psyferblock.io@gmail.com>
2022-06-02 01:58:10 +0300

    eventlistener to button

--- todo.js
+++ todo.js
@@ -1,47 +1,7 @@
-const list_container=document.getElementById('task-list');
-const get_todo_elements=document.getElementById("get-todo-elements");
-let input_field=document.getElementById("input-field");
-
-const todo_items =[];
-
-get_todo_elements.addEventListener("click",(e) =>{
-    e.preventDefault();
-    let text_input=inputfield.textContent;
-    todo_items.push(text_input);
-    console.log(todo_items)
-    debugger
-    addTodo()
-    render()
-    
-});
-
-
-
-
-function addTodo(text) {
-    const todo = {
-      text,
-      id: Date.now().toString(),
-    };
-   
-  
-    todo_items.push(todo);
-    return todo
-};
-
-function clearElement(element){
-    while (element.firstChild){
-        element.removeChild(element.firstChild);
-    }
-}
-  
-function render() {
-    // clearElement(list_container);
-    todo_items.forEach((list)=>{
-        clearElement(list_container);
-        const list_element=document.createElement("li");        
-        list_element.className="list-name";
-        list_element.appendChild(list.value);
-
-    });
-};
+document.getElementById("get-todo-elements").addEventListener("click",(e)=>{
+    let title_field=document.getElementById("title-field")
+    let description_field=document.getElementById("description-field")
+    let priority_field=document.getElementById("priority-field")
+    let date_time_field=document.getElementById("date-time-field")
+    console.log(title_field.value,description_field.value,priority_field.value,date_time_field.value)
+})
