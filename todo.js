83108c76368ec13c2e41f01dc0ccf069664ca172
psyfer <psyferblock.io@gmail.com>
2022-06-02 13:04:36 +0300

    display table

--- todo.js
+++ todo.js
@@ -1,7 +1,49 @@
 document.getElementById("get-todo-elements").addEventListener("click",(e)=>{
+   
     let title_field=document.getElementById("title-field")
     let description_field=document.getElementById("description-field")
     let priority_field=document.getElementById("priority-field")
-    let date_time_field=document.getElementById("date-time-field")
-    console.log(title_field.value,description_field.value,priority_field.value,date_time_field.value)
+    let date_time_field=document.getElementById("date-time-field")  
+    let new_todo_elements ={
+        title:title_field.value,
+        description:description_field.value,
+        priority:priority_field.value,
+        date_time:date_time_field.value,
+    }
+    let id=Date.now();
+    localStorage.setItem(id,JSON.stringify(new_todo_elements))
 })
+
+function displayTable(){
+    let data=localStorage
+    let table_element = document.getElementById("todo-table")
+    let html=""
+    for (const value in data) {
+        if (typeof data[value] === 'string') {
+            info = JSON.parse(data[value])
+            html = html + "<tr><td>"+info.title+"</td><td>"+info.description+"</td><td>"+info.priority+"</td><td>"+info.date_time+"</td></tr>"
+        }
+    }
+    table_element.innerHTML = html
+        
+    }
+displayTable();
+let row =document.querySelectorAll("tr")
+for (const rows in row){
+    if (typeof row[rows] === 'object'){
+    row[rows].addEventListener('click',()=>{
+        if (row[rows].style.textDecoration=="line-through"){
+            row[rows].style.textDecoration="none"
+        }else{
+        row[rows].style.textDecoration ="line-through"
+        }
+    })
+}}  
+
+console.log(row)
+
+    
+
+
+
+
