c769fcbf39be5f8e9b7eea0a94cb2b58f4c71b32
psyfer <psyferblock.io@gmail.com>
2022-06-02 21:25:00 +0300

    cleaning the code

--- todo.js
+++ todo.js
@@ -1,78 +1,101 @@
-let ul_done=document.getElementById("done-table")
+let ul_done=document.getElementById("done-table");
+
+// initialising the list with the adding of new to do elements 
 
 document.getElementById("get-todo-elements").addEventListener("click",(e)=>{
    
-    let title_field=document.getElementById("title-field")
-    let description_field=document.getElementById("description-field")
-    let priority_field=document.getElementById("priority-field")
-    let date_time_field=document.getElementById("date-time-field")  
+    let title_field=document.getElementById("title-field");
+    let description_field=document.getElementById("description-field");
+    let priority_field=document.getElementById("priority-field");
+    let date_time_field=document.getElementById("date-time-field");  
+   
+    // creating an object that holds theinput values from the user 
+
     let new_todo_elements ={
-        title:title_field.value,
+        title:title_field.value,            
         description:description_field.value,
         priority:priority_field.value,
         date_time:date_time_field.value,
-    }
+    };
     let id=Date.now();
-    localStorage.setItem(id,JSON.stringify(new_todo_elements))
-    title_field.value=""
-    description_field.value=""
-    priority_field.value=""
-    date_time_field.value=""
 
-})
+    // storing the items in local storage
+
+    localStorage.setItem(id,JSON.stringify(new_todo_elements));
+    title_field.value="";
+    description_field.value="";
+    priority_field.value="";
+    date_time_field.value="";
+
+});
+
+// creating the display table 
 
 function displayTable(){
-    let data=localStorage
-    let table_element = document.getElementById("todo-table")
-    let html=""
+    let data=localStorage;
+    let table_element = document.getElementById("todo-table");
+    let html="";
     for (const value in data) {
         if (typeof data[value] === 'string') {
-            info = JSON.parse(data[value])
-            html = html + "<tr><td>"+info.title+"</td><td>"+info.description+"</td><td>"+info.priority+"</td><td>"+info.date_time+"</td></tr>"
-        }
-    }
-     table_element.innerHTML = html
+            info = JSON.parse(data[value]);
+            html = html + "<tr><td>"+info.title+"</td><td>"+info.description+"</td><td>"+info.priority+"</td><td>"+info.date_time+"</td></tr>";
+        };
+    };
+     table_element.innerHTML = html;
         
 }
 
 displayTable();
-let row =document.querySelectorAll("tr")
-row.id=`${Date.now()}`
-console.log(row.id)
+
+// creating rows in the display table 
+
+let row =document.querySelectorAll("tr");
+row.id=`${Date.now()}`;
+console.log(row.id);
 for (const rows in row){
     if (typeof row[rows] === 'object'){
         row[rows].addEventListener('click',()=>{
-            moveToDone(row[rows])
-        })  
-    }
-}
+            moveToDone(row[rows]);
+        })  ;
+    };
+};
 console.log(row)
+
+// checks if the items are checked and finished by the user
+
 function alreadyDone(e){
-    return e.classList.contains("striked")
+    return e.classList.contains("striked");
 }
+
+// moves the items checked by user to the done element 
+ 
 function moveToDone(item_to_move){
     if (alreadyDone(item_to_move)){
-        return
-    }
-    item_to_move.classList.add("striked") 
-    let ul_done=document.getElementById("done-table")
-    let li_done=document.createElement("li")
-    let data = item_to_move.querySelectorAll("td")
+        return;
+    };
+    item_to_move.classList.add("striked") ;
+    let ul_done=document.getElementById("done-table");
+    let li_done=document.createElement("li");
+    let data = item_to_move.querySelectorAll("td");
     data.forEach((entry)=>{
-        li_done.innerText+= `${entry.innerText}  `
-    })
-    li_done.style.textDecoration=="line-through"
+        li_done.innerText+= `${entry.innerText}  `;
+    });
+    li_done.style.textDecoration=="line-through";
 
-    ul_done.appendChild(li_done)
+    ul_done.appendChild(li_done);
 
 }
+// sorting function commence
+
 let sort_btn=document.getElementById("sort-btn")
 sort_btn.addEventListener("click",sortTable)
+
+// sorts the tables  based on priority 
 
 function sortTable() {
     let table, rows, switching, i, x, y, shouldSwitch, priority;
     table = document.getElementById("todo-table");
-    rows=document.getElementsByTagName("tr")
+    rows=table.getElementsByTagName("tr")
     priority=document.getElementById("priority-field")
     switching = true;
     
@@ -102,13 +125,14 @@ function sortTable() {
         
         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
-    //   }else{
-    //     rows[i].parentNode.insertAfter(rows[i + 1], rows[i]);
-    //     switching = true;
+      }else{
+        rows[i].parentNode.insertAfter(rows[i + 1], rows[i]);
+        switching = true;
       }
     }
   }
 
+//   search function based on title 
 function searchTodo(){
 
 }
